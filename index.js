3f81a642c39fcfa4eb197aa3516ac677cca3d1c5
hanford <jackhanford@gmail.com>
2018-03-26 00:44:39 -0700

    begin rewriting with workbox-build for less webpack jank

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
-const SwGen = require('./plugin')
 const { GenerateSW } = require('workbox-webpack-plugin')
-const path = require('path')
+const { readDir, readFile } = require('fs-extra')
+const { join } = require('path')
+const SwGen = require('./plugin')
+
+const Precache = require('./next-files')
+// const {generateSW} = require('workbox-build')
 
 module.exports = (nextConfig = {}) => ({
   ...nextConfig,
+  async exportPathMap () {
+    let basePaths = {}
+
+    if (typeof nextConfig.exportPathMap === 'function')  {
+      basePaths = nextConfig.exportPathMap()
+
+      const buildIdPath = join(process.cwd(), '.next', 'BUILD_ID')
+      const buildId = await readFile(buildIdPath, 'utf8')
+      const { precaches } = await Precache(buildId)
+      const urls = precaches.map(({ url }) => url)
+      const swDest = 'out/service-worker.js'
+
+      // globDirectory is intentionally left blank as it's required by workbox
+      // await generateSW({ swDest, importScripts: urls, globDirectory: ' '})
+      // const indexPath = join(process.cwd(), 'out', 'index.html')
+      // const index = await readFile(indexPath, 'utf8')
+      // console.log(index)
+    }
+
+    return basePaths
+  },
   webpack (config, options) {
     if (!options.defaultLoaders) {
       throw new Error('This plugin is not compatible with Next.js versions below 5.0.0 https://err.sh/next-plugins/upgrade')
@@ -13,7 +38,7 @@ module.exports = (nextConfig = {}) => ({
       config = nextConfig.webpack(config, options)
     }
 
-    if (options.dev) return config
+    if (options.dev || options.isServer) return config
 
     const {
       dontAutoRegisterSw = false,
@@ -27,7 +52,7 @@ module.exports = (nextConfig = {}) => ({
     config.plugins = [
       ...config.plugins,
       new GenerateSW({ ...workboxOpts }),
-      new SwGen({ buildId: options.buildId })
+      new SwGen({ buildId: options.buildId, ...workboxOpts })
     ]
 
     const originalEntry = config.entry
